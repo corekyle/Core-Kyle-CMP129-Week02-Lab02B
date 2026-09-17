52f06a0160f332c9c8e68eae11f875cff9d5a7b1
corekyle <core.kyle@student.ccm.edu>
2026-09-17 16:04:48 -0400

    Lab 04B Complete

--- Person.java
+++ Person.java
@@ -1,3 +1,67 @@
-public class Person {
+public class Person 
+{   // START OF Person class
     
-}
+    // Declare fields
+    private String name;
+    private int age;
+    private String email;
+
+    // Defaults contructor
+    public Person()
+    {
+        name = "John Doe";
+        age = 0;
+        email = null;
+    }
+
+    // Contructor with arguments passed
+    public Person(String n, int a, String e)
+    {
+        name = n;
+        age = a;
+        email = e;
+    }
+
+
+    // Person setter methods
+    public void setName(String name)
+    {   // START OF setName method
+        this.name = name;
+    }   // END OF setName method
+
+    public void setAge(int age)
+    {   // START of setAge method
+        this.age = age;
+    }   // END OF setAge method
+
+    public void setEmail(String email)
+    {   // START OF setEmail method
+        this.email = email;
+    }   // END OF setEmail method
+    
+
+    // Person getter methods
+    public String getName()
+    {   // START OF getName method
+        return name;
+    }   // END OF getname method
+
+    public int getAge()
+    {   // START OF getAge method
+        return age;
+    }   // END OF getAge method
+
+    public String getEmail()
+    {   // START OF getEmail method
+        return email;
+    }   // END OF getEmail method
+
+
+    // Method to display the details of a person
+    public void displayInfo()
+    {   // START OF displayInfo method
+        System.out.printf("Name: %s\n" +
+                          "Age: %d\n" +
+                          "Email: %s", name, age, email);
+    }   // END OF displayInfo method
+}   // END OF Person class
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,38 @@
-public class PersonTest {
+import java.util.Scanner;
+
+public class PersonTest 
+{   // START OF PersonTest class
     
-}
+    public static void main(String[] args)
+    {   // START OF main method
+
+        // Create Scanner object
+        Scanner keyboard = new Scanner(System.in);
+
+
+        // Create Person objects
+        Person person1 = new Person();
+        Person person2 = new Person();
+
+
+        // Declare variables
+        String name;
+        int age;
+        String email;
+
+        // Display the info about the Person objects
+        System.out.println("\n" +
+                           "Person 1\n" +
+                           "--------------------");
+        person1.displayInfo();
+
+        System.out.print("\n");
+
+        System.out.println("\n" +
+                           "Person 2\n" +
+                           "--------------------");
+        person2.displayInfo();
+
+    }   // START OF main method
+
+}   // END OF Person Test class
